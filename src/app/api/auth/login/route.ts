51e807c711a8459f5947f4c16623405df3fa591c
rishav76dev <rjrishav037@gmail.com>
2025-07-30 11:07:54 +0530

    bais auth

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,18 @@
+
+
+import { NextResponse } from 'next/server'
+import { prisma } from "@/lib/db";
+import bcrypt from 'bcryptjs'
+
+export async function POST(req: Request) {
+  const { email, password } = await req.json()
+
+  const user = await prisma.user.findUnique({ where: { email } })
+  if (!user) return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
+
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
+
+  // For now just return user info (replace with JWT or session later)
+  return NextResponse.json({ user })
+}
---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+import bcrypt from 'bcryptjs'
+
+export async function POST(req: Request) {
+  const { username, password, email } = await req.json()
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const slug = username.toLowerCase().replace(/\s+/g, '-') + '-' + Math.random().toString(36).substring(2, 6)
+
+  const user = await prisma.user.create({
+    data: {
+        username,email, password: hashedPassword, slug
+    }
+  })
+  return NextResponse.json({user})
+}
