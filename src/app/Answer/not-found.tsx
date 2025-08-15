db4672866620f3768334cdee0c5f7a6f853cf26c
rishav76dev <rjrishav037@gmail.com>
2025-08-15 12:05:17 +0530

    notfound added for answer route

---
+++ src/app/Answer/not-found.tsx
@@ -0,0 +1,30 @@
+// src/app/[slug]/not-found.tsx
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold">
+        This page is not accessible to visitors
+      </h1>
+
+      <p className="text-gray-600 mt-2">
+        If you are a creator,{" "}
+        <Link
+          href="/auth/login"
+          className="text-blue-600 hover:underline font-medium"
+        >
+          log in
+        </Link>{" "}
+        or{" "}
+        <Link
+          href="/auth/register"
+          className="text-blue-600 hover:underline font-medium"
+        >
+          sign up
+        </Link>{" "}
+        to become one.
+      </p>
+    </div>
+  );
+}
