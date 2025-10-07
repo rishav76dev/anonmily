0b50b1256a89f070d0761c6bb5e700308b874fdb
rishav76dev <rjrishav037@gmail.com>
2025-10-07 23:48:43 +0530

    handle cloudinary image

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["example.com"], // <-- add your image domain here
+    domains: ["example.com", "res.cloudinary.com"], // <-- add your image domain here
   },
-
 };
 
 export default nextConfig;
